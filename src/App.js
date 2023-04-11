a8248f652a9f1d57202c7e72b476d974b0dd0e17
MauLight <maulisseluz@gmail.com>
2023-04-10 20:02:19 -0400

    exercise finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, setUser] = React.useState([]);
+
+  const fetchData = () => {
+    const urlUser = fetch('https://randomuser.me/api/?results=1');
+    urlUser.then((response) => {
+      const jsonPromise = response.json();
+      jsonPromise.then((data) => {
+        setUser(data);
+        console.log(data.results)
+      })
+    })
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div style={{padding: "40px"}}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="profile_pic" />
     </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
 
